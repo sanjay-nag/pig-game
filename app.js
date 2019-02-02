86e0312c376d6b555fb7d7a93e3fd59787b5e238
Sanjay Nag <sanjaynag.bangaloreravishankar@sjsu.edu>
2019-02-01 16:01:45 -0800

    Hold functionality

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ GAME RULES:
 
 */
 
-
 var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
@@ -31,7 +30,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	var diceDOM = document.querySelector('.dice');
 	diceDOM.style.display = 'block';
 	diceDOM.src = 'dice-' + dice + '.png';
-	
+
 	//3. Update the round score iff roundNumber is not 1
 	if (dice !== 1) {
 		//Add Score
@@ -39,20 +38,41 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	} else {
 		//Next player
-		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-		roundScore = 0;
-
-		document.getElementById('current-0').textContent = '0';
-		document.getElementById('current-1').textContent = '0';
-
-		document.querySelector('.player-0-panel').classList.toggle('active');
-		document.querySelector('.player-1-panel').classList.toggle('active');
-
-		document.querySelector('.dice').style.display = 'none';
+		nextPlayer();
 	}
 
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	//1. Add Current score to global score
+	scores[activePlayer] += roundScore; 
 
+	//Update the UI
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+
+	//2. Check if player won the game
+	if(scores[activePlayer] >= 20) {
+		document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else {
+		//Next player
+		nextPlayer();
+	}
+});
+
+function nextPlayer() {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+
+	document.querySelector('.dice').style.display = 'none';
+}
 
 
